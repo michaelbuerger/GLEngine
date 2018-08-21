1417ba7cb70566865adc59431f5e9a9299ef692e
Michael Buerger <michaelbuergerart@gmail.com>
2018-08-21 00:58:03 -0700

    Added spike folder for examples/quick tests

---
+++ spike/GLEngineErrors.hpp
@@ -0,0 +1,18 @@
+#ifndef GL_ENGINE_INCLUDE_GL_ENGINE_ERRORS_HPP_
+#define GL_ENGINE_INCLUDE_GL_ENGINE_ERRORS_HPP_
+
+namespace GL_ENGINE_ERRORS { // Place for all non third-party errors to be defined
+// Errors
+
+const char* ErrorMessage(int error)
+{
+	switch (error)
+	{
+	}
+
+	return "Unknown error";
+};
+
+}
+
+#endif 	// GL_ENGINE_INCLUDE_GL_ENGINE_ERRORS_HPP_
