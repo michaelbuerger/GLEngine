4b43c8cc2889de4190408391329c576ba0729ce4
Michael Buerger <michaelbuergerart@gmail.com>
2018-12-29 13:32:12 -0700

    Ignore .gch, helpful Math, CMake Lists

---
+++ include/GLEngine/math/MathFloat.hpp
@@ -0,0 +1,49 @@
+#ifndef MATHFLOAT_HPP
+#define MATHFLOAT_HPP
+
+namespace GLEngine
+{
+	/* Clamp [num] between [min] and [max] */
+	float Clampf(float num, float min, float max)
+	{
+		float num_final = num;
+
+		if (num < min)
+		{
+			num_final = min;
+		} else if(num > max)
+		{
+			num_final = max;
+		}
+
+		return num_final;
+	}
+
+	/* Clamp [num] between [min] and INFINITY */
+	float ClampMinf(float num, float min)
+	{
+		float num_final = num;
+
+		if(num < min)
+		{
+			num_final = min;
+		}
+
+		return num_final;
+	}
+
+	/* Clamp [num] between NEG_INFINITY and [max] */
+	float ClampMaxf(float num, float max)
+	{
+		float num_final = num;
+
+		if (num > max)
+		{
+			num_final = max;
+		}
+
+		return num_final;
+	}
+}
+
+#endif /* end of include guard:MATHFLOAT_HPP */
