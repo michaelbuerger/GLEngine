a31d48bcf0a5365f00fab5cf3d07abdf8c602e5d
popwser <michaelbuergerart@gmail.com>
2019-06-12 01:10:03 -0700

    Transfer 1

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,128 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include "GLEngine/defines.hpp"
+#include "GLEngine/math/math.hpp"
+#include "GLEngine/logging/logging.hpp"
+
 #include <iostream>
 
-int main()
-{
-  std::cout << "Hello World" << std::endl;
+using namespace GLEngine;
+using namespace logging;
+using namespace math;
 
-  return 0;
+int main(void)
+{
+    /* Initialize GLFW and create window */
+    GLFWwindow* window;
+
+    if (!glfwInit())
+        return -1;
+
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, GLE_OPENGL_VERSION_MAJOR);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, GLE_OPENGL_VERSION_MINOR);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    window = glfwCreateWindow(1280, 720, "GLFW Example Window", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    /* Initialize GLEW */
+    GLenum err = glewInit();
+    if (GLEW_OK != err)
+    {
+      std::cout << "Error: " << glewGetErrorString(err) << std::endl;
+    }
+
+    /* Print useful information */
+    std::cout << "Latest supported OpenGL version on this system is " << glGetString(GL_VERSION) << std::endl;
+    std::cout << "GLEngine is currently using OpenGL version " << GLE_OPENGL_VERSION_MAJOR << "." << GLE_OPENGL_VERSION_MINOR << std::endl;
+    std::cout << "Entering loop..." << std::endl;
+
+    float triangle_vertices[] = { // Triangle declared in 3D space
+    -0.5f, -0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+     0.0f,  0.5f, 0.0f
+    };
+
+    uint VBO;
+    glGenBuffers(1, &VBO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0); // Example of unbinding buffer (not needed really)
+
+    uint vertexShader;
+    uint fragmentShader;
+
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+
+    int success;
+    char infoLog[512];
+
+    glCompileShader(vertexShader);
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {
+      glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+      std::cout << "Vertex Shader Compilation Failed\n" << infoLog << std::endl;
+      return -1;
+    }
+
+    glCompileShader(fragmentShader);
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {
+      glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+      std::cout << "Fragment Shader Compilation Failed\n" << infoLog << std::endl;
+      return -1;
+    }
+
+    uint shaderProgram = glCreateProgram();
+
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if(!success) {
+      glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+      std::cout << "Shader Program Linking Failed\n" << infoLog << std::endl;
+      return -1;
+    }
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    glUseProgram(shaderProgram);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    /*
+    *
+    */
+    glEnableVertexAttribArray(0);
+
+    /* Loop */
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
